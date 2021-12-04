23406e1ace564d489575dc1c34074b1d1e2b6eda
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-04 19:26:14 +0700

    add indonesia and arabic language in dataWisata

--- dist/config/data/dataWisata.js
+++ dist/config/data/dataWisata.js
@@ -1,11 +1,26 @@
 const data = [
     {
         id: 1,
-        name: 'Tugu Ikan Belida',
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Belida Fish Monumen',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Tugu Ikan Belido',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'نصب أسماك بليدا',
+                description: 'test',
+            }
+        ],
         latitude: -2.992686,
         longitude: 104.759530,
         thumbnail: '/images/tugu-ikan-belida-1.jpg',
-        description: 'test',
         location: '',
         open: "everyday",
         gallery: [
@@ -16,11 +31,26 @@ const data = [
     },
     {
         id: 2,
-        name: 'Jembatan Ampera',
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Ampera Bridge',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Jembatan Ampera',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'جسر أمبيرا',
+                description: 'test',
+            }
+        ],
         latitude: -2.990377,
         longitude: 104.762298,
         thumbnail: '/images/jembatan-ampera-1.jpg',
-        description: 'test',
         location: '',
         open: "everyday",
         gallery: [
@@ -32,11 +62,26 @@ const data = [
     },
     {
         id: 3,
-        name: 'Kompleks Olahraga jakabaring',
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Jakabaring Sports Complex',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Kompleks Olahraga Jakabaring',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'مجمع جكابرينج الرياضي',
+                description: 'test',
+            }
+        ],
         latitude: -3.017441,
         longitude: 104.798137,
         thumbnail: '/images/jakabaring-sport-city-1.jpg',
-        description: 'test',
         location: '',
         open: [
             {
@@ -74,11 +119,26 @@ const data = [
     },
     {
         id: 4,
-        name: 'Benteng Kuto Besak',
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Kuto Besak Fortress',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Benteng Kuto Besak',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'قلعة كوتو باشاك',
+                description: 'test',
+            }
+        ],
         latitude: -2.991408,
         longitude: 104.759163,
         thumbnail: '/images/benteng-kuto-besak-1.jpg',
-        description: 'test',
         location: '',
         open: "everyday",
         gallery: [

6b32faea02ff7a79e762ebfa8c6e19d9164191cd
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-05 22:22:49 +0700

    add new data and new images to dataWisata.js

--- dist/config/data/dataWisata.js
+++ dist/config/data/dataWisata.js
@@ -146,6 +146,284 @@ const data = [
             '/images/benteng-kuto-besak-2.jpg',
             '/images/benteng-kuto-besak-3.jpg'
         ]
+    },
+    {
+        id: 5,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Punti Kayu Amusement Park Palembang',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Taman Punti Kayu Palembang',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'متنزه بونتي كايو الترفيهي باليمبانج',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9437208,
+        longitude: 104.7260844,
+        thumbnail: '/images/punti-kayu-1.jpg',
+        location: '',
+        open: [
+            {
+                day: 'monday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '15:00:00.000 +0700'
+            },
+            {
+                day: 'tuesday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '15:00:00.000 +0700'
+            },
+            {
+                day: 'wednesday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '15:00:00.000 +0700'
+            },
+            {
+                day: 'thursday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '15:00:00.000 +0700'
+            },
+            {
+                day: 'friday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '15:44:00.000 +0700'
+            },
+            {
+                day: 'saturday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '16:44:00.000 +0700'
+            },
+            {
+                day: 'sunday',
+                openTime: '09:00:00.000 +0700',
+                closeTime: '16:44:00.000 +0700'
+            },
+        ],
+        gallery: [
+            '/images/punti-kayu-1.jpg',
+            '/images/punti-kayu-2.jpg',
+            '/images/punti-kayu-3.jpg'
+        ]
+    },
+    {
+        id: 6,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Palembang Bird Park',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Taman Burung Palembang',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'حديقة باليمبانج للطيور',
+                description: 'test',
+            }
+        ],
+        latitude: -3.0364581,
+        longitude: 104.7881957,
+        thumbnail: '/images/bird-park-1.jpg',
+        location: '',
+        open: [
+            {
+                day: 'monday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'tuesday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'wednesday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'thursday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'friday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'saturday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+            {
+                day: 'sunday',
+                openTime: '10:00:00.000 +0700',
+                closeTime: '18:00:00.000 +0700'
+            },
+        ],
+        gallery: [
+            '/images/bird-park-1.jpg',
+            '/images/bird-park-2.jpg',
+            '/images/bird-park-3.jpg'
+        ]
+    },
+    {
+        id: 6,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Bukit Siguntang',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Bukit Siguntang',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'بوكيت سيغونتانغ',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9950144,
+        longitude: 104.7173889,
+        thumbnail: '/images/bukit-siguntang-1.jpg',
+        location: '',
+        open: "everyday",
+        gallery: [
+            '/images/bukit-siguntang-1.jpg',
+            '/images/bukit-siguntang-2.jpg',
+            '/images/bukit-siguntang-3.jpg'
+        ]
+    },
+    {
+        id: 7,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Balaputradeva Museum',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Museum Balaputradeva',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'متحف بالابوتراديوا',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9950144,
+        longitude: 104.7173889,
+        thumbnail: '/images/museum-balaputradewa-1.jpg',
+        location: '',
+        open: "everyday",
+        gallery: [
+            '/images/museum-balaputradewa-1.jpg',
+            '/images/museum-balaputradewa-2.jpg'
+        ]
+    },
+    {
+        id: 8,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: 'Sultan Mahmud Badaruddin II Museum',
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: 'Museum Sultan Mahmud Badaruddin II',
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'متحف السلطان محمود بدر الدين الثاني',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9950144,
+        longitude: 104.7173889,
+        thumbnail: '/images/museum-sultan-mahmud-badarudin-II-1.jpg',
+        location: '',
+        open: "everyday",
+        gallery: [
+            '/images/museum-sultan-mahmud-badarudin-II-1.jpg',
+            '/images/museum-sultan-mahmud-badarudin-II-2.jpg'
+        ]
+    },
+    {
+        id: 9,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: "Bayt Al-qur'an Palembang",
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: "Bayt Al-qur'an Palembang",
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'بيت القرآن باليمبانج',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9950144,
+        longitude: 104.7173889,
+        thumbnail: '/images/bayt-alquran-1.jpg',
+        location: '',
+        open: "everyday",
+        gallery: [
+            '/images/bayt-alquran-1.jpg',
+            '/images/bayt-alquran-2.png'
+        ]
+    },
+    {
+        id: 10,
+        differentLanguage: [
+            {
+                language: 'en',
+                name: "Taman Kambang Iwak Besak",
+                description: 'test',
+            },
+            {
+                language: 'in',
+                name: "Kambang Iwak Besak Park",
+                description: 'test',
+            },
+            {
+                language: 'ar',
+                name: 'حديقة بيساك كامبانج إيواك',
+                description: 'test',
+            }
+        ],
+        latitude: -2.9950144,
+        longitude: 104.7173889,
+        thumbnail: '/images/kambang-iwak-1.jpg',
+        location: '',
+        open: "everyday",
+        gallery: [
+            '/images/kambang-iwak-1.jpg',
+            '/images/kambang-iwak-2.jpg'
+        ]
     }
 ]
 

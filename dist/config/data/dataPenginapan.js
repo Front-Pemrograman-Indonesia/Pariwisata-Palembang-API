78f023dd2fbec039a1d917f456cb44cc2420340a
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-06 00:45:31 +0700

    add new data to dataPenginapan

--- dist/config/data/dataPenginapan.js
+++ dist/config/data/dataPenginapan.js
@@ -1,7 +1,7 @@
 const data = [
     {
         id: 1,
-        name: 'favehotel Palembang',
+        name: 'Favehotel Palembang',
         latitude: -2.956123,
         longitude: 104.746106,
         thumbnail: '/images/hotel-favehotel-1.jpg',
@@ -11,12 +11,12 @@ const data = [
     },
     {
         id: 2,
-        name: 'Hotel Aston',
+        name: 'Aston Hotel Palembang',
         latitude: -2.954645,
         longitude: 104.747101,
-        thumbnail: '/images/hotel-aston-1.jpg',
+        thumbnail: '/images/hotel-aston-1.jpeg',
         gallery: [
-            '/images/hotel-aston-1.jpg'
+            '/images/hotel-aston-1.jpeg'
         ]
     },
     {
@@ -27,6 +27,176 @@ const data = [
         thumbnail: '/images/hotel-beston-1.jpg',
         gallery: [
             '/images/hotel-beston-1.jpg'
+        ]
+    },
+	{
+        id: 4,
+        name: 'The Alts Hotel Palembang',
+        latitude: -2.9709533,
+        longitude: 104.7624986,
+        thumbnail: '/images/hotel-thealts-1.jpeg',
+        gallery: [
+            '/images/hotel-thealts-1.jpeg'
+        ]
+    },
+	{
+        id: 5,
+        name: 'Aryaduta Hotel Palembang',
+        latitude: -2.9772474,
+        longitude: 104.7388166,
+        thumbnail: '/images/hotel-aryaduta-1.jpg',
+        gallery: [
+            '/images/hotel-aryaduta-1.jpg'
+        ]
+    },
+	{
+        id: 6,
+        name: 'The Excelton Hotel Palembang',
+        latitude: -2.9705388,
+        longitude: 104.7283792,
+        thumbnail: '/images/hotel-theexcelton-1.jpeg',
+        gallery: [
+            '/images/hotel-theexcelton-1.jpeg'
+        ]
+    },
+	{
+        id: 7,
+        name: 'Harper Hotel Palembang',
+        latitude: -2.9524111,
+        longitude: 104.7552087,
+        thumbnail: '/images/hotel-harper-1.jpg',
+        gallery: [
+            '/images/hotel-harper-1.jpg'
+        ]
+    },
+	{
+        id: 8,
+        name: 'Batiqa Hotel Palembang',
+        latitude: -2.9845675,
+        longitude: 104.7419177,
+        thumbnail: '/images/hotel-batiqa-1.jpg',
+        gallery: [
+            '/images/hotel-batiqa-1.jpg'
+        ]
+    },
+	{
+        id: 9,
+        name: 'Santika Hotel Palembang',
+        latitude: -2.9810489,
+        longitude: 104.7459899,
+        thumbnail: '/images/hotel-santika-1.jpg',
+        gallery: [
+            '/images/hotel-santika-1.jpg'
+        ]
+    },
+	{
+        id: 10,
+        name: 'Novotel Palembang',
+        latitude: -2.9499047,
+        longitude: 104.762283,
+        thumbnail: '/images/hotel-novotel-1.jpg',
+        gallery: [
+            '/images/hotel-novotel-1.jpg'
+        ]
+    },
+	{
+        id: 11,
+        name: 'Amaris Hotel Palembang',
+        latitude: -2.9628857,
+        longitude: 104.7339847,
+        thumbnail: '/images/hotel-amaris-1.jpg',
+        gallery: [
+            '/images/hotel-amaris-1.jpg'
+        ]
+    },
+	{
+        id: 12,
+        name: 'Classie Hotel Palembang',
+        latitude: -2.9751781,
+        longitude: 104.7625706,
+        thumbnail: '/images/hotel-classie-1.jpg',
+        gallery: [
+            '/images/hotel-classie-1.jpg'
+        ]
+    },
+	{
+        id: 13,
+        name: 'The Arista Hotel Palembang',
+        latitude: -2.9778309,
+        longitude: 104.7442853,
+        thumbnail: '/images/hotel-thearista-1.jpg',
+        gallery: [
+            '/images/hotel-thearista-1.jpg'
+        ]
+    },
+	{
+        id: 14,
+        name: 'Algoritma Hotel Palembang',
+        latitude: -2.9699083,
+        longitude: 104.7678102,
+        thumbnail: '/images/hotel-algoritma-1.jpeg',
+        gallery: [
+            '/images/hotel-algoritma-1.jpeg'
+        ]
+    },
+	{
+        id: 15,
+        name: 'MaxOneHotels Palembang',
+        latitude: -2.9516348,
+        longitude: 104.7572144,
+        thumbnail: '/images/hotel-maxonehotels-1.jpg',
+        gallery: [
+            '/images/hotel-maxonehotels-1.jpg'
+        ]
+    },
+	{
+        id: 16,
+        name: 'The 1O1 Hotel Palembang',
+        latitude: -2.972184,
+        longitude: 104.7624327,
+        thumbnail: '/images/hotel-the1o1-1.jpg',
+        gallery: [
+            '/images/hotel-the1o1-1.jpg'
+        ]
+    },
+	{
+        id: 17,
+        name: 'Sintesa Peninsula Hotel Palembang',
+        latitude: -2.9509152,
+        longitude: 104.7816637,
+        thumbnail: '/images/hotel-sintesapeninsula-1.jpg',
+        gallery: [
+            '/images/hotel-sintesapeninsula-1.jpg'
+        ]
+    },
+	{
+        id: 18,
+        name: 'Anugerah Hotel Palembang',
+        latitude: -2.9742959,
+        longitude: 104.7504271,
+        thumbnail: '/images/hotel-anugerah-1.jpg',
+        gallery: [
+            '/images/hotel-anugerah-1.jpg'
+        ]
+    },
+	{
+        id: 19,
+        name: 'All Nite & Day Hotel Palembang',
+        latitude: -2.9755521,
+        longitude: 104.7639747,
+        thumbnail: '/images/hotel-allniteandday-1.jpg',
+        gallery: [
+            '/images/hotel-allniteandday-1.jpg'
+        ]
+    },
+	{
+        id: 20,
+        name: 'Emilia Hotel Palembang',
+        latitude: -2.9850301,
+        longitude: 104.7526131,
+        thumbnail: '/images/hotel-emilia-1.jpg',
+        gallery: [
+            '/images/hotel-emilia-1.jpg'
         ]
     }
 ]

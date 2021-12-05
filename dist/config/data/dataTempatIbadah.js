03901f9ebecb5a98496c82734e7ca469caf08437
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-05 23:37:29 +0700

    update data tempat ibadah

--- dist/config/data/dataTempatIbadah.js
+++ dist/config/data/dataTempatIbadah.js
@@ -27,6 +27,329 @@ const data = [
         longitude: 104.757670,
         idTypeTempatIbadah: 1
     },
+    {
+        id: 5,
+        name: 'Masjid Lawang Kidul',
+        latitude: -2.98088,
+        longitude: 104.77550,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 6,
+        name: 'Masjid Kiai Marogan',
+        latitude: -3.01477,
+        longitude: 104.75005,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 7,
+        name: 'Masjid Sultan Agung',
+        latitude: -2.98194,
+        longitude: 104.79341,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 8,
+        name: 'Gereja Katolik Santo Petrus Paroki Sako Kenten',
+        latitude: -2.9374,
+        longitude: 104.79597,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 9,
+        name: 'Gereja Katolik Hati Kudus',
+        latitude: -2.98338,
+        longitude: 104.75968,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 10,
+        name: 'Gereja Katholik St Yosef',
+        latitude: -2.97472,
+        longitude: 104.75383,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 11,
+        name: 'Gereja Katolik St. Fransiskus de Sales',
+        latitude: -2.96656,
+        longitude: 104.78617,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 12,
+        name: 'Gereja Protestan Pertamina RU III',
+        latitude: -2.99539,
+        longitude: 104.82300,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 13,
+        name: 'Gereja Kristen Protestan Indonesia (GKPI) Resort Palembang II',
+        latitude: -2.91913,
+        longitude: 104.70239,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 14,
+        name: 'GPIB Pniel Palembang',
+        latitude: -2.95644,
+        longitude: 104.78481,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 15,
+        name: 'GPIB Immanuel Palembang',
+        latitude: -2.99197,
+        longitude: 104.75247,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 16,
+        name: 'Gereja Kristen Batak Protestan Distrik Sumbagsel',
+        latitude: -2.96845,
+        longitude: 104.76180,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 17,
+        name: 'Gereja HKBP Sukarami Palembang',
+        latitude: -2.93433,
+        longitude: 104.70232,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 18,
+        name: 'GKPA (Gereja Kristen Protestan Angkola) Palembang',
+        latitude: -2.94301,
+        longitude: 104.76392,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 19,
+        name: 'Gereja Protestan Di Indonesia Bagian Barat Immanuel',
+        latitude: -2.99101,
+        longitude: 104.75668,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 20,
+        name: 'Maha Wihara Dharmakirti',
+        latitude: -2.97171,
+        longitude: 104.74855,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 21,
+        name: 'Vihara Giok Poh Tian',
+        latitude: -2.98500,
+        longitude: 104.75673,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 22,
+        name: 'Vihara Amitabha Graha',
+        latitude: -2.95669,
+        longitude: 104.77534,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 23,
+        name: 'Vihara Samanthabhadra',
+        latitude: -2.96983,
+        longitude: 104.77158,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 24,
+        name: 'Vihara Vimalakirti',
+        latitude: -2.96293,
+        longitude: 104.77567,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 25,
+        name: 'Vihara Sari Putra',
+        latitude: -2.97418,
+        longitude: 104.75723,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 26,
+        name: 'Vihara Dhamma Vijja',
+        latitude: -2.94301,
+        longitude: 104.76551,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 27,
+        name: 'Vihara Liong Hian King',
+        latitude: -2.94750,
+        longitude: 104.70736,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 28,
+        name: 'Vihara Gunung Dewata',
+        latitude: -2.97204,
+        longitude: 104.74982,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 29,
+        name: 'Vihara Hok Liong Tong',
+        latitude: -2.98883,
+        longitude: 104.73420,
+        idTypeTempatIbadah: 4
+    },
+    {
+        id: 30,
+        name: 'Pura Agung Sriwijaya',
+        latitude: -2.94609,
+        longitude: 104.76522,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 31,
+        name: 'Pura Kahyangan Swarna Dwipa Jakabaring',
+        latitude: -3.00881,
+        longitude: 104.79054,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 32,
+        name: 'Sekretariat PD KMHDI Sumatera Selatan',
+        latitude: -2.94725,
+        longitude: 104.76173,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 33,
+        name: 'PURA KAYANGAN TUNGGAL MERTA SARI',
+        latitude: -2.93376,
+        longitude: 104.00243,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 34,
+        name: 'Pura Puseh',
+        latitude: -3.85568,
+        longitude: 104.83317,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 35,
+        name: 'Pura Umat Hindu',
+        latitude: -3.78733,
+        longitude: 104.90225,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 36,
+        name: 'Pura Banjar Mertasari',
+        latitude: -3.76137,
+        longitude: 104.89698,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 37,
+        name: 'Pura Khayangan Tunggal Dharma Wanasari',
+        latitude: -3.76195,
+        longitude: 104.90128,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 38,
+        name: 'Pura Bonenak',
+        latitude: -3.80692,
+        longitude: 104.86143,
+        idTypeTempatIbadah: 5
+    },
+
+    {
+        id: 39,
+        name: 'Pura Merajan Sebuluh Sanggeng',
+        latitude: -3.80978,
+        longitude: 104.86364,
+        idTypeTempatIbadah: 5
+    },
+    {
+        id: 40,
+        name: 'GEREJA PROTESTAN INJILI NUSANTARA',
+        latitude: -2.96347,
+        longitude: 104.74590,
+        idTypeTempatIbadah: 3
+    },
+    {
+        id: 41,
+        name: 'Masjid Al Falah',
+        latitude: -2.98498,
+        longitude: 104.75771,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 42,
+        name: 'Masjid Baitul Iman',
+        latitude: -2.98324,
+        longitude: 104.75617,
+        idTypeTempatIbadah: 1
+    },   
+    {
+        id: 43,
+        name: 'Masjid. Ridwanullah',
+        latitude: -2.99100,
+        longitude: 104.75667,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 44,
+        name: 'Masjid Baitul Majid',
+        latitude: -99216,
+        longitude: 104.74912,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 45,
+        name: 'Masjid Al-Ghazali',
+        latitude: -2.98823,
+        longitude: 104.73361,
+        idTypeTempatIbadah: 1
+    },
+    {
+        id: 46,
+        name: 'Gereja Katedral St. Maria Palembang',
+        latitude: -2.98824,
+        longitude: 104.74717,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 47,
+        name: 'Gereja Katolik Santa Maria Ratu Rosario',
+        latitude: -2.99663,
+        longitude: 104.77354,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 48,
+        name: 'Catholic Church of St. John Paul II Jakabaring',
+        latitude: -3.00895,
+        longitude: 104.79034,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 49,
+        name: 'Gereja Katolik St. Paulus Paroki Plaju',
+        latitude: -2.99718,
+        longitude: 104.81862,
+        idTypeTempatIbadah: 2
+    },
+    {
+        id: 50,
+        name: 'Kapel Katolik Km 7',
+        latitude: -2.94019,
+        longitude: 104.72667,
+        idTypeTempatIbadah: 2
+    },
 ]
 
 module.exports = data;

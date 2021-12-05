7523cdb8263abe92676e89fcd27e7b5e34c33f7c
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-05 14:05:22 +0700

    create calculateDistanceThenSetLanguage

---
+++ dist/config/lib/calculateDistanceThenSetLanguage.js
@@ -0,0 +1,12 @@
+Math.radians = (deg) => deg * (Math.PI / 180);
+
+const calculateDistance = (latitude, longitude, placeLatitude, placeLongitude, language) => {
+    const distance =  (6371 * Math.acos( Math.cos( Math.radians(parseFloat(latitude)) ) * 
+        Math.cos( Math.radians( placeLatitude )) * 
+        Math.cos( Math.radians( placeLongitude ) - Math.radians(parseFloat(longitude)) ) + Math.sin( Math.radians(parseFloat(latitude)) ) * 
+        Math.sin( Math.radians( placeLatitude ))));
+    
+    return language === 'ar'? (Math.round(distance * 100) / 100).toLocaleString('ar-EG'): (Math.round(distance * 100) / 100);
+}
+
+module.exports = calculateDistance;

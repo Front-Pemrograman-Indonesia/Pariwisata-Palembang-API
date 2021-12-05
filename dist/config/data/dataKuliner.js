13d2bfa4ab8e047c1df675653b9c50edf65caa22
Satria Aluh Perwira Nusa <satrianusa10@gmail.com>
2021-12-05 22:52:52 +0700

    update / add many data to dataKuliner.js

--- dist/config/data/dataKuliner.js
+++ dist/config/data/dataKuliner.js
@@ -19,6 +19,86 @@ const data = [
             '/images/RM-Pindang-Musi-Rawas.jpg'
         ]
     },
+    {
+        id: 3,
+        name: 'Pempek Taman Kenten',
+        latitude: -2.954674,
+        longitude: 104.773176,
+        thumbnail: '/images/Pempek-Taman-Kenten.jpeg',
+        gallery: [
+            '/images/Pempek-Taman-Kenten.jpeg'
+        ]
+    },
+    {
+        id: 4,
+        name: 'Saoenk Kito Restaurant',
+        latitude: -2.974866,
+        longitude: 104.726082,
+        thumbnail: '/images/Saoenk-Kito-Restaurant.jpg',
+        gallery: [
+            '/images/Kedai-Tiga-Nyonya.jpg'
+        ]
+    },
+    {
+        id: 5,
+        name: 'RM Pondok Kelapo',
+        latitude: -2.963709,
+        longitude: 104.735866,
+        thumbnail: '/images/RM-Pondok-Kelapo.jpg',
+        gallery: [
+            '/images/RM-Pondok-Kelapo.jpg'
+        ]
+    },
+    {
+        id: 6,
+        name: 'Youwie Coffe & Eatery',
+        latitude: -2.992400,
+        longitude: 104.783913,
+        thumbnail: '/images/Youwie-Coffe-and-Eatery.jpeg',
+        gallery: [
+            '/images/Youwie-Coffe-and-Eatery.jpeg'
+        ]
+    },
+    {
+        id: 7,
+        name: 'Pecel Lele Mas Imam',
+        latitude: -2.992872,
+        longitude: 104.798598,
+        thumbnail: '/images/Pecel-Lele-Mas-Imam.jpeg',
+        gallery: [
+            '/images/Pecel-Lele-Mas-Imam.jpeg'
+        ]
+    },
+    {
+        id: 8,
+        name: 'Wisata Resto Kolam Susu',
+        latitude: -2.942063,
+        longitude: 104.802180,
+        thumbnail: '/images/Wisata-Resto-Kolam-Susu.jpg',
+        gallery: [
+            '/images/Wisata-Resto-Kolam-Susu.jpg'
+        ]
+    },
+    {
+        id: 9,
+        name: 'Pondok Pindang Umak',
+        latitude: -2.920427,
+        longitude: 104.707748,
+        thumbnail: '/images/Pondok-Pindang-Umak.jpg',
+        gallery: [
+            '/images/Pondok-Pindang-Umak.jpg'
+        ]
+    },
+    {
+        id: 10,
+        name: 'Rumah Makan Talago Minang',
+        latitude: -2.942008,
+        longitude: 104.801645,
+        thumbnail: '/images/Rumah-Makan-Talago-Minang.jpeg',
+        gallery: [
+            '/images/Rumah-Makan-Talago-Minang.jpeg'
+        ]
+    },
 ]
 
 module.exports = data;
